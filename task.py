859343984ef59297c291ca284fe33caafe5e799a
Rishabh Choudhari <86475634+RishabhSanjayChoudhari@users.noreply.github.com>
2026-08-24 22:35:44 +0530

    Add initial structure for job description processing

---
+++ task.py
@@ -0,0 +1,7 @@
+#1) take skills input and store
+#2) enter job description
+#3) list out skills in JD
+#AllSkills = ['c','c++','python','java']
+#userSkills = ['c++']
+#JD  = "efficient in pyhton & java"
+#js = ['python','java']
